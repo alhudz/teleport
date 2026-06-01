292013ee9660e6d52c546a849a5d1b4aa96d60a6
joerger <bjoerger@goteleport.com>
2026-06-01 11:38:51 -0700

    Include error in span; remove TracerProvider hook.

--- lib/srv/regular/sshserver_test.go
+++ lib/srv/regular/sshserver_test.go
@@ -1574,7 +1574,7 @@ func x11EchoSession(ctx context.Context, t *testing.T, clt *tracessh.Client) x11
 
 	// Handle any x11 channel requests received from the server
 	// and start x11 forwarding to the client display.
-	err = clt.HandleChannelOpen(ctx, x11.ChannelRequest, func(ctx context.Context, ch ssh.NewChannel) {
+	err = clt.HandleChannelOpen(ctx, x11.ChannelRequest, func(ctx context.Context, ch ssh.NewChannel) error {
 		sch, sin, err := ch.Accept()
 		assert.NoError(t, err)
 		defer sch.Close()
@@ -1595,6 +1595,8 @@ func x11EchoSession(ctx context.Context, t *testing.T, clt *tracessh.Client) x11
 		if !errors.Is(err, context.Canceled) {
 			assert.NoError(t, err)
 		}
+
+		return nil
 	})
 	require.NoError(t, err)
 
